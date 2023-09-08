673d906950ffefe6b9307dd852cc43be208576b3
Diabene <addodiabene69@gmail.com>
2023-09-08 13:14:46 +0000

    cors local

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,1 +1,1 @@
-export const allowedOrigins = ["https://dbn-portfolio.netlify.app/", "http://localhost:3000"];
+export const allowedOrigins = ["https://dbn-portfolio.netlify.app/", "http://localhost:5173"];
