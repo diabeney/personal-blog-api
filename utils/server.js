5e73682a64e00f26a2de2f9001fa6c9f84d503c2
Diabene <addodiabene69@gmail.com>
2023-09-23 18:42:51 +0000

    start server

--- utils/server.js
+++ utils/server.js
@@ -4,15 +4,28 @@ import ConnectDB from "./db.js";
 
 config();
 
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 
 const app = express();
 
-ConnectDB(process.env.MONGO_URI);
-// ConnectDB("mongodb://127.0.0.1:27017");
+// ConnectDB(process.env.MONGO_URI);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+(async function startServer(port) {
+  await ConnectDB("mongodb://127.0.0.1:27017");
+  const server = app.listen(port);
+  server.on("listening", async () => {
+    console.info("Server listening on port:", port);
+  });
+  server.on("error", (err) => {
+    if (err.code === "EADDRINUSE") {
+      const newPort = port + 1;
+      console.warn(`Port ${port} already in use, trying ${newPort} instead.`);
+      setTimeout(() => {
+        PORT = process.env.PORT || newPort;
+        startServer(newPort);
+      }, 1000);
+    }
+  });
+})(PORT);
 
 export default app;
